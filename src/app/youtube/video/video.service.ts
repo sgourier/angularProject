1ad27332f4b2e58dadea0ce83f8598a795176d45
Benjamin Hil <benjam.hil@gmail.com>
2017-06-22 19:13:54 +0200

    #youtube :  get videos

--- src/app/youtube/video/video.service.ts
+++ src/app/youtube/video/video.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {YOUTUBE_CONFIG} from '../youtubeSettings';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class VideoService {
 
-  constructor() { }
+  data: any = {};
+  constructor(private http: Http) { }
 
+  searchQueryVideos(query) {
+    return this.http.get(YOUTUBE_CONFIG.apiUrl + 'search?part=snippet&order=viewCount&q=' + query + '&type=video&videoDefinition=high&key=' + YOUTUBE_CONFIG.apiKey)
+        .map((res: Response) => res.json())
+        .subscribe(data => {
+          this.data = data;
+          console.log(this.data);
+        })
+  }
 }
