c9a1657c87d7bdbed40df093414772ce986eabd4
Benjamin Hil <benjam.hil@gmail.com>
2017-06-22 14:26:41 +0200

    #youtube :  adding playlist

--- src/app/youtube/playlist/playlist.service.ts
+++ src/app/youtube/playlist/playlist.service.ts
@@ -13,7 +13,9 @@ export class PlaylistService {
   data: any = {};
 
   constructor(private http: Http) {
-    this.accessToken = localStorage.getItem('access_token');
+    this.accessToken = localStorage.getItem('youtube_access_token');
+    this.getVideosByPlaylistId();
+    this.createPlaylist();
   }
 
   getPlaylistsByChannelId(username) {
@@ -22,7 +24,6 @@ export class PlaylistService {
              this.http.get(YOUTUBE_CONFIG.apiUrl + 'playlists?part=snippet%2CcontentDetails&channelId=' + data.json()['items'][0]['id'] + '&key=' + YOUTUBE_CONFIG.apiKey)
                       .map((res: Response) => res.json())
                       .subscribe(playlist => {
-                          console.log(playlist);
                           this.data = playlist;
                       })
           });
@@ -31,5 +32,32 @@ export class PlaylistService {
   getChannelId(username) {
         return this.http.get(YOUTUBE_CONFIG.apiUrl + 'channels?part=id&forUsername=' + username + '&key=' + YOUTUBE_CONFIG.apiKey)
             .toPromise();
+  }
+
+  getUserPlaylist() {
+        return this.http.get(YOUTUBE_CONFIG.apiUrl + 'playlists?access_token=' + this.accessToken + '&part=snippet&mine=true')
+            .toPromise();
+  }
+
+  getVideosByPlaylistId() {
+   this.getUserPlaylist()
+       .then(data => {
+           this.http.get(YOUTUBE_CONFIG.apiUrl + 'playlistItems?part=snippet&playlistId=' + data.json()['items'][0]['id'])
+               .map((res: Response) => res.json())
+               .subscribe(videos => {
+                   console.log(videos);
+                   this.data = videos;
+               })
+       })
+  }
+
+  createPlaylist() {
+      this.http.post(YOUTUBE_CONFIG.apiUrl + 'playlists?access_token=' + this.accessToken + 'part=snippet%2Cstatus', { 'snippet': {
+          'title': 'OKLM',
+          'description': 'New playlist description'
+      },
+          'status': {
+              'privacyStatus': 'private'
+          }})
   }
 }
